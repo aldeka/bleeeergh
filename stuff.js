a113f6c0d5d875962591541b72329756fbe3b751
Karen Rustad <karen.rustad@gmail.com>
2012-08-25 13:19:33 -0700

    yay, pictures

--- stuff.js
+++ stuff.js
@@ -35,9 +35,9 @@ MyApp.addInitializer(function(options){
     
 $(document).ready(function(){
     var cats = new AngryCats([
-        new AngryCat ({ name: 'Wet Cat'}),
-        new AngryCat ({ name: 'Bitey'}),
-        new AngryCat ({ name: 'Ceiling Cat'})
+        new AngryCat ({ name: 'Wet Cat', image_path: 'assets/images/cat2.jpg'}),
+        new AngryCat ({ name: 'Bitey', image_path: 'assets/images/cat1.jpg'}),
+        new AngryCat ({ name: 'Ceiling Cat', image_path: 'assets/images/cat3.jpg'})
         ]);
     MyApp.start({ cats: cats });
     });
