23f9e5792f0bee490782442484556dd1b5f5b6e9
Karen Rustad <karen.rustad@gmail.com>
2012-08-25 13:56:34 -0700

    voting and swapping, huzzah

--- stuff.js
+++ stuff.js
@@ -5,23 +5,95 @@ MyApp.addRegions({
     });
     
 AngryCat = Backbone.Model.extend({
+    rankUp: function(){ 
+        this.set({rank: this.get('rank') - 1 });
+        },
+    rankDown: function(){
+        this.set({ rank: this.get('rank') + 1 });
+        },
+    addVote: function(){
+        this.set('votes', this.get('votes') + 1);
+        },
+    defaults: {
+        votes: 0
+        }
     });
 
 AngryCats = Backbone.Collection.extend({
     model: AngryCat,
     initialize: function(cats){
         var rank = 1;
+        var self = this;
         _.each(cats, function(cat){
             cat.set('rank', rank);
             ++ rank;
             });
+            
+        this.on('add', function(cat){
+            if( !cat.get('rank') ){
+                var error = Error("Cat must have a rank defined before being added to the collection");
+                error.name = "NoRankError";
+                throw error;
+                }
+            });
+        
+        MyApp.vent.on('rank:up', function(cat){
+            if (cat.get('rank') == 1){
+                return true;
+                }
+            self.rankUp(cat);
+            self.sort();
+            });
+            
+        MyApp.vent.on('rank:down', function(cat){
+            if (cat.get('rank') == self.size()){
+                return true;
+                }
+            self.rankDown(cat);
+            self.sort();
+            });
+            
+        },
+    comparator: function(cat){
+        return cat.get('rank');
+        },
+        
+    rankUp: function(cat){
+        var self = this;
+        var rankToSwap = cat.get('rank') - 1;
+        var otherCat = this.at(rankToSwap - 1);
+        cat.rankUp();
+        otherCat.rankDown();
+        },
+    rankDown: function(cat){
+        var self = this;
+        var rankToSwap = cat.get('rank') + 1;
+        var otherCat = this.at(rankToSwap - 1);
+        
+        cat.rankDown();
+        otherCat.rankUp();
         }
     });
     
 AngryCatView = Backbone.Marionette.ItemView.extend({
     template: "#angry_cat-template",
     tagName: 'tr',
-    className: 'angry_cat'
+    className: 'angry_cat',
+    events: {
+        'click .rank_up img': 'rankUp',
+        'click .rank_down img': 'rankDown'
+        },
+    rankUp: function(){
+        this.model.addVote();
+        MyApp.vent.trigger('rank:up', this.model);
+        },
+    rankDown: function(){
+        this.model.addVote();
+        MyApp.vent.trigger('rank:down', this.model);
+        },
+    initialize: function(){
+        this.bindTo(this.model,"change:votes", this.render);
+        }
     });
     
 AngryCatsView = Backbone.Marionette.CompositeView.extend({
@@ -31,18 +103,7 @@ AngryCatsView = Backbone.Marionette.CompositeView.extend({
     template: '#angry_cats-template',
     itemView: AngryCatView,
     
-    appendHtml: function(collectionView, itemView) { collectionView.$('tbody').append(itemView.el); },
-    
-    events: {
-        'click .rank_up img': 'rankUp',
-        'click .rank_down img': 'rankDown'
-        },
-    rankUp: function(){
-        console.log('rank up');
-        },
-    rankDown: function(){
-        console.log('rank down');
-        }
+    appendHtml: function(collectionView, itemView) { collectionView.$('tbody').append(itemView.el); }
     });
     
 MyApp.addInitializer(function(options){
@@ -59,4 +120,10 @@ $(document).ready(function(){
         new AngryCat ({ name: 'Ceiling Cat', image_path: 'assets/images/cat3.jpg'})
         ]);
     MyApp.start({ cats: cats });
+    
+    cats.add(new AngryCat({ 
+        name: 'Cranky Cat', 
+        image_path: 'assets/images/cat4.jpg', 
+        rank: cats.size() + 1 
+        }));
     });
